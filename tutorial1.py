944e3a8e21485818bc03af32cf29996f9d0befec
BYUMVUHORE Aimable <aimablebyumvuhore@gmail.com>
2024-10-19 14:52:42 +0200

    adding video

--- tutorial1.py
+++ tutorial1.py
@@ -1,18 +1,17 @@
 import cv2
 
-# Read the image
 img = cv2.imread('assets/bai.jpg', -1)
-# 0 for grayscale, -1 for original color
 
-# Resize the image to half the size
+# Resize the image
 img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
 
-# Rotate the image 90 degrees clockwise
+# Rotate the image
 img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 
+# Save the modified image
 cv2.imwrite('new_img.jpg', img)
 
-# Display the image in a window
+# Display the image
 cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
